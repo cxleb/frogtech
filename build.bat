8f6cd1f72e2f7ce251fee78f950482b68bfee0b3
Caleb <waitcaleb@gmail.com>
2022-06-14 12:20:35 +1000

    try incremental link

--- build.bat
+++ build.bat
@@ -4,5 +4,5 @@ set source=Source\Runtime\Runtime.cpp Source\Runtime\VulkanGpu.cpp Source\Runtim
 
 set libs="%vk_sdk_path%\Lib\GenericCodeGen.lib" "%vk_sdk_path%/Lib/glslang.lib" "%vk_sdk_path%/Lib/HLSL.lib" "%vk_sdk_path%/Lib/MachineIndependent.lib" "%vk_sdk_path%/Lib/OGLCompiler.lib" "%vk_sdk_path%/Lib/OSDependent.lib" "%vk_sdk_path%/Lib/SPIRV.lib" "%vk_sdk_path%/Lib/SPIRV-Tools.lib" "%vk_sdk_path%/Lib/SPIRV-Tools-opt.lib" "%vk_sdk_path%/Lib/SPVRemapper.lib" "%vk_sdk_path%/Lib/vulkan-1.lib"
 rem "%vk_sdk_path%/Lib/glslang-default-resource-limits.lib"
-cl /nologo /EHsc /MD /std:c++20 %includes% %source% %libs% user32.lib
+cl /nologo /EHsc /MD /std:c++20 %includes% %source% %libs% user32.lib /link /INCREMENTAL
 
